95517d0fbfa00e73de0153c7b22e988ba1b3dc67
Joscha Feth <joscha@feth.com>
2025-03-10 13:22:24 +0000

    fix: only parse error response if it comes as JSON

--- dlt_source_morphais/rest_client.py
+++ dlt_source_morphais/rest_client.py
@@ -55,17 +55,18 @@ def get_rest_client(
 
 def raise_if_error(response: Response, *args: Any, **kwargs: Any) -> None:
     if response.status_code < 200 or response.status_code >= 300:
-        try:
-            error = error_adapter.validate_json(response.text)
-            response.reason = error.error
-            # TODO: This is not great at all, an error code and/or a 204 response would be a lot better
-            if error.error == "There are no startups available.":
-                response.status_code = 204  # No Content
-                response._content = dumps([]).encode("utf-8")
-        except ValidationError as e:
-            logging.error(e)
-            logging.debug(response.text)
-            raise e
+        if response.headers.get("Content-Type", "").startswith("application/json"):
+            try:
+                error = error_adapter.validate_json(response.text)
+                response.reason = error.error
+                # TODO: This is not great at all, an error code and/or a 204 response would be a lot better
+                if error.error == "There are no startups available.":
+                    response.status_code = 204  # No Content
+                    response._content = dumps([]).encode("utf-8")
+            except ValidationError as e:
+                logging.error(e)
+                logging.debug(response.text)
+                raise e
         response.raise_for_status()
 
 
